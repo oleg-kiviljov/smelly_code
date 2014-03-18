8f23287566b1593a74b769438664b7f36948f07d
Oleg Kiviljov <eproplay@gmail.com>
2014-03-18 17:02:35 +0200

    more modifications

--- app/assets/javascripts/sliding-panel.js
+++ app/assets/javascripts/sliding-panel.js
@@ -1,24 +1,16 @@
-$(document).on('click', '.panel-heading span.clickable', function (e) {
-  var $this = $(this);
-  if (!$this.hasClass('panel-collapsed')) {
-    $this.parents('.panel').find('.panel-body').slideUp();
-    $this.addClass('panel-collapsed');
-    $this.find('i').removeClass('glyphicon-minus').addClass('glyphicon-plus');
-  } else {
-    $this.parents('.panel').find('.panel-body').slideDown();
-    $this.removeClass('panel-collapsed');
-    $this.find('i').removeClass('glyphicon-plus').addClass('glyphicon-minus');
-  }
-});
 $(document).on('click', '.panel div.clickable', function (e) {
   var $this = $(this);
-  if (!$this.hasClass('panel-collapsed')) {
-    $this.parents('.panel').find('.panel-body').slideUp();
-    $this.addClass('panel-collapsed');
-    $this.find('i').removeClass('fa-minus').addClass('fa-plus');
-  } else {
+  if ($this.hasClass('panel-collapsed')) {
     $this.parents('.panel').find('.panel-body').slideDown();
     $this.removeClass('panel-collapsed');
     $this.find('i').removeClass('fa-plus').addClass('fa-minus');
+  } else {
+    $this.parents('.panel').find('.panel-body').slideUp();
+    $this.addClass('panel-collapsed');
+    $this.find('i').removeClass('fa-minus').addClass('fa-plus');
   }
+});
+
+$(document).on("ready page:load", function(){
+  $('.panel-heading.clickable').addClass('panel-collapsed').parent('.panel').find('.panel-body').hide()
 });
